2a1915f83dae3991c9da5496ad5a7ae549d254a8
Daniela Moreno <dani9292@hotmail.com>
2023-08-09 03:46:38 -0500

    create method for create new operations in controller

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,2 +1,29 @@
 class OperationsController < ApplicationController
+    def new
+        @group = Group.find(params[:group_id])
+        @operation = Operation.new
+      end
+    
+      def create
+        @group = Group.find(params[:group_id])
+        @operation = Operation.new(operation_params)
+        @operation.author_id = current_user.id
+    
+        if @operation.save
+            selected_group = Group.find(params[:operation][:group_id])
+            selected_group.operations << @operation
+    
+          flash[:success] = 'Operation was successfully created.'
+          redirect_to user_group_path(current_user, selected_group.id)
+        else
+          flash.now[:alert] = 'Error: Operation could not be created.'
+          render 'new'
+        end
+      end
+    
+      private
+    
+      def operation_params
+        params.require(:operation).permit(:name, :amount)
+      end
 end
