0a2b4c3805248cb298cdffdde4e6e9eab557bc90
Daniela Moreno <dani9292@hotmail.com>
2023-08-09 12:55:04 -0500

    add validations to the operation class

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,4 +2,8 @@ class Operation < ApplicationRecord
     belongs_to :author, class_name: "User", foreign_key: "author_id"
     has_many :group_operations
     has_many :groups, through: :group_operations
+
+    validates :name, presence: true
+    validates :amount, presence: true
+
 end
