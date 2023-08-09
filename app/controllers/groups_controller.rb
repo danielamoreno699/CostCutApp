e010e42911a6bd93f10ac338e4d4d8db4f5be1b1
Daniela Moreno <dani9292@hotmail.com>
2023-08-09 03:44:33 -0500

    create method for index group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,7 @@
 class GroupsController < ApplicationController
+    include GroupIconOptions
+
+    def index
+      @groups = Group.includes(:author).where(author_id: current_user.id)
+    end
 end
