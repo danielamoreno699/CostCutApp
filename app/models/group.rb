9677d415779fdb081967d59e1f55642be8cccc33
Daniela Moreno <dani9292@hotmail.com>
2023-08-09 13:02:25 -0500

    add validations for group class

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,8 @@ class Group < ApplicationRecord
     has_many :group_operations
     has_many :operations, through: :group_operations
     validates :name, presence: true, uniqueness: { scope: :author_id, message: 'You already have a group with this name', case_sensitive: false }
-
+    validates :icon, presence:true
+    
 def sum_amount
     operations.sum(:amount)
 end
