542f3f1dc4466408cac1df9d41d2b2c2afb7cdf5
Daniela Moreno <dani9292@hotmail.com>
2023-08-10 21:29:26 -0500

    change ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    class Ability
-      include CanCan::Ability
-    
-      def initialize(user)
-        user ||= User.new 
-    
-        return unless user.id.present?
-    
-        can :manage, Group, author_id: user.id
-        can :manage, Operation, author_id: user.id
-      end
-    end
+    user ||= User.new # Assuming User is your user model class
+
+    # Only logged in users can manage categories and expenses
+    return unless user.id.present?
+
+    can :manage, Group, author_id: user.id
+    can :manage, Movement, author_id: user.id
   end
 end
